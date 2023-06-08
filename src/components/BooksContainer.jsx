4beabd9ef158d839169b622e9475ef91c26ee1f2
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-08 23:57:11 +0600

    Add index and main file

---
+++ src/components/BooksContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddBook from './Addbook';
+import Books from './books';
+
+const BooksContainer = () => (
+  <div>
+    <AddBook />
+    <Books />
+  </div>
+);
+
+export default BooksContainer;
