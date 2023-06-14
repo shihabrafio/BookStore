61f227b883863a678b856ed6030be44edcd855eb
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-14 14:51:07 +0600

    Add bookslice

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,26 +1,50 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuid } from 'uuid';
+import initialbook from './initialbooks';
+
+const storedBooks = () => {
+  const storedBooks = localStorage.getItem('booksdata');
+  const storedBooksArr = JSON.parse(storedBooks);
+
+  if (storedBooks) {
+    if (storedBooksArr.length !== 0) {
+      return storedBooksArr;
+    }
+    return initialbook;
+  }
+  return initialbook;
+};
+
+const initialState = {
+  books: storedBooks(),
+};
 
 const bookSlice = createSlice({
   name: 'books',
-  initialState: {
-    books: [],
-  },
+  initialState,
   reducers: {
-    addBook: (state, action) => ({
-      ...state,
-      books: [
-        {
-          id: action.payload.id,
-          title: action.payload.title,
-        },
-      ],
-    }),
-    removeBook: (state, action) => ({
-      ...state,
-      books: state.books.filter((book) => book.id !== action.payload.id),
-    }),
+    addBook: (state, action) => {
+      const bookTitle = action.payload[0];
+      const bookAuthor = action.payload[1];
+      const newBook = {
+        item_id: uuid(),
+        title: bookTitle,
+        author: bookAuthor,
+        category: 'Action',
+      };
+      return {
+        ...state,
+        books: [...state.books, newBook],
+      };
+    },
+    removeBook: (state, action) => {
+      const id = action.payload;
+      const newstate = { ...state };
+      newstate.books = state.books.filter((book) => book.item_id !== id);
+      return newstate;
+    },
   },
 });
 
 export const { addBook, removeBook } = bookSlice.actions;
-export const bookReducer = bookSlice.reducer;
+export default bookSlice.reducer;
