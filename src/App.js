aeb39c2492a375deea044e39ad3c4b4903e80910
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-14 14:51:56 +0600

    Add configure file

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import BooksContainer from './components/BooksContainer';
 import Categories from './components/Categories';
 import Navbar from './components/Navbar';
+import store from './redux/store';
 
 const App = () => (
-  <Router>
+  <Provider store={store}>
     <Navbar />
     <Routes>
       <Route path="/" element={<BooksContainer />} />
       <Route path="/categories" element={<Categories />} />
     </Routes>
-  </Router>
+  </Provider>
 );
 
 export default App;
