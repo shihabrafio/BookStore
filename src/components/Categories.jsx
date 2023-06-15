5b6a1ef7ff94c526bb8dd83a20a16cf3c1601485
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-06-16 01:42:40 +0600

    Fix linter Error

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Categories = () => (
-  <button type="submit">Check Status</button>
+  <H1>Under Construction</H1>
 );
 
 export default Categories;
+
+const H1 = styled.h1`
+  font-size: 40px;
+  text-align: center;
+`;
